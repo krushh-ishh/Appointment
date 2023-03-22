c0332e87981cef2be6dfe36cbba2752ec500546f
Kashish <Aman Kumar@AmanKumar-PC>
2023-03-22 14:57:33 +0530

    poc Spring boot starter

---
+++ appointment/src/main/java/com/poc/appointment/controller/AppointmentAppStarter.java
@@ -0,0 +1,13 @@
+package com.poc.appointment.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentAppStarter {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(AppointmentAppStarter.class, args);
+	}
+}
