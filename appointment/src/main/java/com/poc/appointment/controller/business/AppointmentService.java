d1276bc2cbf49139d39ec38c8ea74b6e8ba6e816
Kashish <Aman Kumar@Kashish-PC>
2023-03-26 11:59:05 +0530

    .

--- appointment/src/main/java/com/poc/appointment/controller/business/AppointmentService.java
+++ appointment/src/main/java/com/poc/appointment/controller/business/AppointmentService.java
@@ -1,6 +1,8 @@
 package com.poc.appointment.controller.business;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -10,11 +12,11 @@ import com.poc.appointment.entity.Appointment;
 @Service
 public class AppointmentService {
 
-	private List<Appointment> appointments =  Arrays.asList(
+	private List<Appointment> appointments =  new ArrayList<>(Arrays.asList(
 			new Appointment(1,"kas","a","s","s"),
 			new Appointment(2,"kas","a","s","s"),
 			new Appointment(3,"kas","a","s","s"),
-			new Appointment(4,"kas","a","s","s"));
+			new Appointment(4,"kas","a","s","s")));
 	
 	
 	public List<Appointment> getAllAppointments(){
@@ -29,6 +31,23 @@ public class AppointmentService {
 	public void addAppointment(Appointment appointment) {
 		 appointments.add(appointment);
 		
+	}
+
+	public void updateAppointment(Appointment appointment, int id) {
+		
+		for (int i =0; i<appointments.size();i++) {
+			Appointment appointment2 = appointments.get(i);
+			if(appointment2.getAppointmentId() == id) {
+				appointments.set(i, appointment2);
+				return;
+			}
+			
+		}
+		
+	}
+
+	public void deleteAppointment(int id) { 
+		appointments.removeIf(a-> a.getAppointmentId()==id);
 	}
 }
 
